f5475c9fa84f1082670fcd4545afbe79ff99db09
rahul_raval <rahulraval1997@gmail.com>
2018-03-14 19:23:26 +0530

    scrapping details from uvpce.ac.in

--- web-scapping/scrape.py
+++ web-scapping/scrape.py
@@ -1,37 +1,91 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import os
 
-# url = 'http://convert2mp3.net/en/'
-#
-# convert_url_base =  'http://convert2mp3.net/en/index.php?p=tags&id=youtube_Qvd43XKY-f0&key=bXDdu6wH0S1E'
-# url2 = 'http://convert2mp3.net/en/index.php?p=complete&id=youtube_Qvd43XKY-f0&key=Tu4V6ocZeocS'
-# youtube_url = 'https://www.youtube.com/watch?v=Qvd43XKY-f0'
-# # requesting to that url
+# uvpce_staff = 'http://www.uvpce.ac.in/ce-faculty'
+# url_r = requests.get(uvpce_staff)
+# print(url_r.status_code)
 
+# # make it readable using soup
+# data_soup = BeautifulSoup(url_r.text, 'html.parser')
+# # table of faculty details
+# table = data_soup.find('table',{'class': 'views-view-grid cols-1'})
+# tbody = table.tbody
+# print(tbody.prettify())
 
-uvpce_staff = 'http://www.uvpce.ac.in/ce-faculty'
-url_r = requests.get(uvpce_staff)
-print(url_r.status_code)
-# print(url_r.text)
+def downlad_img(img_list):
+    download_path = '/home/codal/mywork/python-practice/practice/web-scapping/images/'
+    img_name = ""
+    print("Downloading images . . . ")
+    for img_link in img_list:
+        img = requests.get(img_link)
+        ext = '.jpg'
+        img_name = (img_link.split('/')[-1].split('.')[0]) + ext
 
-# make it readable using soup
-data_soup = BeautifulSoup(url_r.text, 'html.parser')
-table = data_soup.findAll('table',{'class': 'views-view-grid cols-1'})
-faculty_details =[]
+        # if image already exists then skip
+        if os.path.exists(download_path+img_name):
+            pass
+        else:
+            # print(img_name)
+            with open(os.path.join(download_path,img_name),'wb') as file:
+                # for chunk in img.iter_content(1024):
+                #     file.write(chunk)
+                file.write(img.content)
+    print("Done")
 
-for tab in table:
-    tr_table = tab.findAll('tr')
+def scarp_faculty_details():
+    faculty_details =[]
+    img_list = []
 
-    for row in tr_table:
+    uvpce_faculty = 'http://www.uvpce.ac.in/ce-faculty'
+    url_r = requests.get(uvpce_faculty)
+
+    # make it readable using soup
+    data_soup = BeautifulSoup(url_r.text, 'html.parser')
+    # table of faculty details
+    table = data_soup.find('table',{'class': 'views-view-grid cols-1'})
+    tbody = table.tbody
+
+    print("Scrapping faculty details . . .")
+    for tr in tbody.contents:
         obj = {}
-        # print(row)
-        name = row.find('span', {'class':'field-content'}).text
-        obj['name'] = name.strip(' ')
-        faculty_details.append(obj)
-        # print(text)
-        img_src = row.find('img', src = True).get('src')
-        # print(img_src)
-print(faculty_details)
+        if tr.name == None:
+            continue
+        try :
+            # profile Image link
+            # print(tr.td.find('div',{'class': 'views-field-field-profile-image'}).img.get('src').split('?')[0])
+            obj['img-src'] = tr.td.find('div',{'class': 'views-field-field-profile-image'}).img.get('src').split('?')[0]
+            img_list.append(obj['img-src'])
+            # faculty-name
+            # print()
+            obj['name'] = tr.td.find('div',{'class': 'views-field-title'}).text.strip(' \n\t')
 
-# download_url = data_soup.findAll('a',{'class': 'btn-success'}, href =True)
-# print(download_url[0].get('href'))
+            # faculty-designation
+            # print()
+            obj['designation'] = tr.td.find('div',{'class': 'views-field-field-present-designation'}).text.strip(' \n\t')
+
+            # faculty-extension-no
+            # print()
+            obj['extension-no'] = tr.td.find('div',{'class': 'views-field-field-extension-no'}).div.text.strip(' \n\t')
+
+            # faculty-email
+            # print()
+            obj['email'] = tr.td.find('div',{'class': 'views-field-field-email-address'}).text.strip(' \n\t')
+
+            # facult-research-interests
+            # print()
+            obj['research-interests'] = tr.td.find('div',{'class': 'views-field-field-research-interests'}).div.text.strip(' \n\t Title:\xa0')
+        except:
+            pass
+        faculty_details.append(obj)
+        # print(faculty_details)
+        # print(img_list)
+
+    # store the data into json file
+    with open('faculty_details.json','w+') as file:
+        file.write(json.dumps(faculty_details, indent = 2))
+
+    downlad_img(img_list)
+
+scarp_faculty_details()
